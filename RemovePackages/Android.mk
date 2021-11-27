2ed6f9117b3401952a4c3eca1b1653457f481685
drkphnx <dark.phnx12@gmail.com>
2021-11-27 20:05:47 +0530

    alioth: Include debloater
    Alioth: Remove SoundAmplifier and musicfx from debloater
    
    * We are not using Dolby so lets build prebuild MusicFx and soundamplifier
    
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,58 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    AudioFX \
+    Eleven \
+    Twelve \
+    Etar \
+    ExactCalculator \
+    Jelly \
+    Matlog \
+    MusicFX \
+    Music \
+    Recorder \
+    Seedvault \
+    Chrome \
+    Chrome-Stub \
+    Drive \
+    DeskClock \
+    Stk \
+    GoogleCamera \
+    Glimpse \
+    Gallery2 \
+    MiuiScanner \
+    Matlog \
+    MatLog \
+    MyVerizonServices \
+    OBDM_Permissions \
+    GoogleTTS \
+    Videos \
+    MaestroPrebuilt \
+    talkback \
+    SoundAmplifierPrebuilt \
+    DevicePolicyPrebuilt \
+    TrichromeLibrary \
+    TrichromeWebView \
+    TrichromeChrome \
+    obdm_stub \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    SafetyHubPrebuilt \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    SprintDM \
+    SprintHM \
+    ViaBrowser \
+    Videos \
+    VZWAPNLib \
+    VzwOmaTrigger
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
