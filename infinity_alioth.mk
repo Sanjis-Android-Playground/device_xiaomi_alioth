9ecf7029048b489bb437df811dd2d941a5cc6b9b
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2025-06-04 10:37:24 +0000

    alioth: lets explore infinity
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- infinity_alioth.mk
+++ infinity_alioth.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit Camera-related flags
 TARGET_USES_MIUI_CAMERA := true
@@ -18,7 +18,7 @@ TARGET_INCLUDES_MIUI_CAMERA := true
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := infinity_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -27,3 +27,9 @@ PRODUCT_MODEL := POCO F3
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.221114.001/V816.0.2.0.TKHMIXM:user/release-keys
+# Infinity-X Specific FlagsAdd commentMore actions
+WITH_GAPPS := true
+USE_MOTO_CALCULATOR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+INFINITY_MAINTAINER := ꜱᴀɴᴊɪ
+TARGET_SUPPORTS_BLUR := true
