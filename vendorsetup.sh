b57364853946634a5e2e61b5de08f66af313c209
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2025-06-10 09:09:19 +0000

    updatevt

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
 
-git clone git@github.com:Sanjis-Android-Playground/device_xiaomi_sm8250-common.git -b 15-qpr2 device/xiaomi/sm8250-common
+git clone git@github.com:Sanjis-Android-Playground/device_xiaomi_sm8250-common.git -b axion device/xiaomi/sm8250-common
 git clone git@github.com:Sanjis-Android-Playground/kernel_sm8250-16.1.0.git --depth=1 kernel/xiaomi/sm8250
 cd kernel/xiaomi/sm8250
 git submodule init
@@ -16,6 +16,5 @@ git clone https://github.com/Roynas-Android-Playground/hardware_samsung-extra_in
 rm -rf hardware/xiaomi
 git clone git@github.com:Sanjis-Android-Playground/hardware_xiaomi.git -b 15-qpr2 hardware/xiaomi
 #git clone https://gitlab.com/johnmart19/vendor_xiaomi_camera.git vendor/xiaomi/camera
-rm -rf packages/resources/devicesettings
-git clone git@github.com:Sanjis-Android-Playground/packages_resources_devicesettings.git packages/resources/devicesettings
 git clone https://gitlab.com/dark.phnx12/vendor_xiaomi_camera vendor/xiaomi/camera
+git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379.git prebuilts/clang/host/linux-x86/clang-r547379
