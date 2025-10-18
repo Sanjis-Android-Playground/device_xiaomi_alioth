b4d54048f7676049028082f36e108fd44e724c12
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2025-10-18 13:24:05 +0000

    alioth: Infinte ways
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- infinity_alioth.mk
+++ infinity_alioth.mk
@@ -9,16 +9,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit Camera-related flags
 TARGET_USES_MIUI_CAMERA := true
 TARGET_INCLUDES_MIUI_CAMERA := true
 
+# Infinity-X Specific FlagsAdd commentMore actions
+WITH_GAPPS := true
+USE_MOTO_CALCULATOR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+INFINITY_MAINTAINER := ꜱᴀɴᴊɪ
+TARGET_SUPPORTS_BLUR := true
+
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := infinity_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
