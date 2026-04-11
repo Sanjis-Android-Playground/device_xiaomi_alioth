fc1ea3a164115c6e13ccfc2bbc6f56760f2f2f58
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2026-04-11 10:16:15 +0000

    normal thermal

--- parts/src/org/lineageos/xiaomiparts/data/ThermalUtils.kt
+++ parts/src/org/lineageos/xiaomiparts/data/ThermalUtils.kt
@@ -229,7 +229,7 @@ private constructor(
         ),
         DEFAULT(
             8,
-            "20", // thermal-mgame.conf [zenzer0rs: changed from normal.conf to mgame.conf, because mgame has lower frequency all cores are locked at 1.8GHz while normal.conf goes up to 2.4GHz]
+            "0", // thermal-mgame.conf [zenzer0rs: changed from normal.conf to mgame.conf, because mgame has lower frequency all cores are locked at 1.8GHz while normal.conf goes up to 2.4GHz]
             "thermal.default=",
             R.string.thermal_default,
         ),
