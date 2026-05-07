26e509ebce7a663965c3fd3d7eaa5b052cd62448
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2026-05-07 12:20:59 +0000

    alioth: inherit signing keys

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ TARGET_IS_VAB := true
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
+#Signing keys
+$(call inherit-product, vendor/lunaris-priv/keys/keys.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
